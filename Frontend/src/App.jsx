cf9a42ae124305ab8404da7f06d5776e52d263e1
nishu847 <nishusharma.sharma2006@gmail.com>
2024-08-11 22:17:28 +0530

    creators page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Featured from "./Pages/Featured"
+import Created from "./Pages/Created"
 function App() {
 
   return (
    <>
    <Featured/>
+   <Created/>
    </>
   )
 }
