be47c528c5494ba94d932c6e378acfcbc82fe6e6
nishu847 <nishusharma.sharma2006@gmail.com>
2025-05-13 15:53:15 +0530

    Fix: removed unused import or restored Featured component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react"
-import Featured from "./Pages/Featured"
 import Navbar from "./Components/Navbar"
 import Footer from "./Components/Footer"
 function App() {
 
   return (
    <>
-   {/* <Featured/>
    {/* <Navbar/> */}
     {/* <Footer/> */}
    </>
