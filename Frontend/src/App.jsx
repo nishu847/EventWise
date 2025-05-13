c9f905785bf45b285202443eca30a8917e0428f4
nishu847 <nishusharma.sharma2006@gmail.com>
2025-05-13 15:26:05 +0530

    new

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import Featured from "./Pages/Featured"
-import Created from "./Pages/Created"
 import Navbar from "./Components/Navbar"
 import Footer from "./Components/Footer"
 function App() {
